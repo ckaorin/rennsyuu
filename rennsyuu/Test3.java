b072e4964227ec8ce25e7dfec63fef07b66487ec
kaori <vistlip.ttruy@gmail.com>
2017-02-22 22:15:52 +0900

    komattara Ctrl+C

---
+++ rennsyuu/Test3.java
@@ -0,0 +1,9 @@
+public class Test3{
+	public static void main(String []args){
+		String name = "tomoki";
+			while (name.equals("tomoki")){
+				System.out.println("Hey!tomoki!");
+				System.out.println("whileを使ってみたよ");
+			}
+	}
+}	
