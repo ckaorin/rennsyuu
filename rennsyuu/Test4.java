96be3689d64d38540badf179b1d122f290f6cbe5
kaori <vistlip.ttruy@gmail.com>
2017-02-22 23:53:15 +0900

    nennrei

--- rennsyuu/Test4.java
+++ rennsyuu/Test4.java
@@ -1,12 +1,13 @@
 public class Test4{
 	public static void main(String[]args){
 		System.out.println("あなたの年齢を入力してください。");
-		String yourage = new java.util.Scanner ( System.in ).nextLine ();
-			int n = Integer.parseInt(yourage);
-			if ( n > 22 ){
+		int yourage = new java.util.Scanner ( System.in ).nextInt ();
+			if ( yourage > 22 ){
 				System.out.println("私よりも年上ですね！");
-			}else{
-				System.out.println("私と同い年か年下ですね！");
+			} else if ( yourage == 22){
+				System.out.println("私と同い年ですね！");
+			} else {
+				System.out.println("私よりも年下ですね！");
 		}
 	}
 }
